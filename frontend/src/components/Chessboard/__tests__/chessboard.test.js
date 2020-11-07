8330da722f891f7300f985e7fc2e801fc1374814
Jiri Petrlik <jiripetrlik@gmail.com>
2020-11-07 22:38:35 +0100

    Snapshot test for Chessboard component

---
+++ frontend/src/components/Chessboard/__tests__/chessboard.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Chessboard from '../Chessboard'
+
+test('snapshot test', () => {
+    const size = 8
+    const player = "white"
+    const pieces = {
+        Whitepieces: [{X: 0, Y: 0}, {X: 0, Y: 2}],
+        Blackpieces: [{X: 6, Y: 2}, {X: 5, Y: 3}, {X: 4, Y: 4}],
+        Whitequeens: [{X: 4, Y: 6}],
+        Blackqueens: [{X: 6, Y: 6}, {X: 4, Y: 4}],
+    }
+
+    const tree = renderer
+        .create(<Chessboard size={size} player={player} pieces={pieces} />)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});

cadf0b86011c9730e76c3c0c7dce0f3e51a21f04
Jiri Petrlik <jiripetrlik@gmail.com>
2020-11-11 10:52:06 +0100

    Refactor function pieceType

--- frontend/src/logic/index.js
+++ frontend/src/logic/index.js
@@ -1,34 +1,27 @@
+function containsPiece(row, column, pieceArray) {
+    for (const i in pieceArray) {
+        const coordinates = pieceArray[i]
+
+        if (coordinates.X === row && coordinates.Y === column) {
+            return true
+        }
+    }
+
+    return false
+}
+
 function pieceType(row, column, pieces) {
-    for (const i in pieces.Whitepieces) {
-        const coordinates = pieces.Whitepieces[i]
-
-        if (coordinates.X === row && coordinates.Y === column) {
-            return "white-piece"
-        }
+    if (containsPiece(row, column, pieces.Whitepieces)) {
+        return "white-piece"
     }
-
-    for (const i in pieces.Blackpieces) {
-        const coordinates = pieces.Blackpieces[i]
-
-        if (coordinates.X === row && coordinates.Y === column) {
-            return "black-piece"
-        }
+    if (containsPiece(row, column, pieces.Blackpieces)) {
+        return "black-piece"
     }
-
-    for (const i in pieces.Whitequeens) {
-        const coordinates = pieces.Whitequeens[i]
-
-        if (coordinates.X === row && coordinates.Y === column) {
-            return "white-queen"
-        }
+    if (containsPiece(row, column, pieces.Whitequeens)) {
+        return "white-queen"
     }
-
-    for (const i in pieces.Blackqueens) {
-        const coordinates = pieces.Blackqueens[i]
-
-        if (coordinates.X === row && coordinates.Y === column) {
-            return "black-queen"
-        }
+    if (containsPiece(row, column, pieces.Blackqueens)) {
+        return "black-queen"
     }
 
     return "none"

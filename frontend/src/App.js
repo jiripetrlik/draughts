c50ed08095c3b61b9b95d89b23f55551a2bc2947
Jiri Petrlik <jiripetrlik@gmail.com>
2020-11-18 18:54:10 +0100

    Delete obsolete part of initial state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -13,34 +13,6 @@ class App extends React.Component {
 
     this.state = {
       Status: "login",
-
-      Players: {
-        waiting: [
-          { id: 1, name: "player1" },
-          { id: 2, name: "player2" },
-          { id: 3, name: "player3" },
-          { id: 4, name: "player4" },
-        ],
-        playing: [
-          { id: 1, names: ["Player 7", "Player 8"] },
-          { id: 2, names: ["Player 9", "Player 10"] },
-          { id: 3, names: ["Player 11", "Player 12"] },
-        ]
-      },
-  
-      Pieces: {
-        whitepieces: [[5, 4], [7, 7]],
-        blackpieces: [[3, 4]],
-        whitequeens: [[1, 1]],
-        blackqueens: [[6, 6], [4, 4]],
-        moves: [[2, 2], [2, 3]]
-      },
-
-      Messages: [
-        { name: "Opponent", text: "Hello" },
-        { name: "You", text: "Hi!" },
-        { name: "Opponent", text: "How are you?" },
-      ]
     }
   }
 

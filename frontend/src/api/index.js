76ea2de9948f0c6d3c8db2e8d891fdc0671bf933
Jiri Petrlik <jiripetrlik@gmail.com>
2020-12-21 18:11:02 +0100

    Enable to resolve web sockets URL

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,4 +1,13 @@
-var socket = new WebSocket("ws://localhost:8080/ws");
+let wsURL = window.location.href
+wsURL = wsURL.replace("http://", "ws://")
+wsURL = wsURL.replace("https://", "wss://")
+wsURL = wsURL + "ws"
+
+if (process.env.NODE_ENV === "development") {
+    wsURL = "ws://localhost:8080/ws"
+}
+
+let socket = new WebSocket(wsURL);
 
 let connect = changeState => {
     socket.onmessage = msg => {

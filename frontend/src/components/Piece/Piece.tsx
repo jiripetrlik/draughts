078b900f023d8bde0e3bb6d4fb65d840da4f04d9
Jiri Petrlik <jiripetrlik@gmail.com>
2020-12-22 20:03:25 +0100

    Fix chessboard square size

--- frontend/src/components/Piece/Piece.tsx
+++ frontend/src/components/Piece/Piece.tsx
@@ -10,13 +10,13 @@ class Piece extends React.Component<Readonly<any>, Readonly<any>> {
     render() {
         let image
         if (this.props.type === "white-piece") {
-            image = <img src={whitepiece} alt=""/>
+            image = <img src={whitepiece} alt="" className="piece"/>
         } else if (this.props.type === "black-piece") {
-            image = <img src={blackpiece} alt=""/>
+            image = <img src={blackpiece} alt="" className="piece"/>
         } else if (this.props.type === "white-queen") {
-            image = <img src={whitequeen} alt=""/>
+            image = <img src={whitequeen} alt="" className="piece"/>
         } else if (this.props.type === "black-queen") {
-            image = <img src={blackqueen} alt=""/>
+            image = <img src={blackqueen} alt="" className="piece"/>
         } else if (this.props.type === "move") {
             image = <img src={move} alt=""/>
         }

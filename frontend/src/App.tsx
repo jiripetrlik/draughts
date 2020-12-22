45b18810b08ad9e02f3f96ac8dc698aeac6608d4
Jiri Petrlik <jiripetrlik@gmail.com>
2020-12-22 18:29:13 +0100

    Introduce network error dialog

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContentText from '@material-ui/core/DialogContentText';
 import './App.css';
 import Game from "./components/Game/Game"
 import NameInput from "./components/NameInput/NameInput"
@@ -12,14 +18,32 @@ class App extends React.Component<Readonly<any>, Readonly<any>> {
     super(props)
 
     this.state = {
+      connectionDialogOpen: false,
       Status: "login",
     }
+
+    this.handleConnectionReset = this.handleConnectionReset.bind(this)
   }
 
   componentDidMount() {
+    this.connectToNetwork()
+  }
+
+  handleConnectionReset() {
+    this.connectToNetwork()
+    this.setState({
+      connectionDialogOpen: false,
+      Status: "login"
+    })
+  }
+
+  connectToNetwork() {
     connect((msg: any) => {
       let state = JSON.parse(msg.data)
       this.setState(state)
+    },
+    () => {
+      this.setState({connectionDialogOpen: true})
     })
   }
 
@@ -41,6 +65,17 @@ class App extends React.Component<Readonly<any>, Readonly<any>> {
         {this.state.Status === "game" &&
           <Game player={this.state.Color} nextMove={this.state.NextMove} pieces={this.state.Pieces} messages={this.state.Messages} />
         }
+        <Dialog open={this.state.connectionDialogOpen} onClose={this.handleConnectionReset}>
+            <DialogTitle id="alert-dialog-title">{"Connection error"}</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                Unable to connect to the backend server. Connection will be restarted.
+              </DialogContentText>
+              <DialogActions>
+                <Button onClick={this.handleConnectionReset}>Restart connection</Button>
+              </DialogActions>              
+            </DialogContent>
+        </Dialog>
       </div>
     );
   }
--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -7,16 +7,27 @@ if (process.env.NODE_ENV === "development") {
     wsURL = "ws://localhost:8080/ws"
 }
 
-let socket = new WebSocket(wsURL);
+let socket = null;
+let onerror = null;
 
-let connect = changeState => {
+let connect = (changeState, errorFunction) => {
+    socket = new WebSocket(wsURL)
+    onerror = errorFunction
+
     socket.onmessage = msg => {
         changeState(msg);
     };
+    socket.onerror = error => {
+        errorFunction(error)
+    }
 }
 
 let sendMessage = msg => {
-    socket.send(msg)
+    if (socket.readyState === 1) {
+        socket.send(msg)
+    } else {
+        onerror()
+    }
 }
 
 export {connect, sendMessage}

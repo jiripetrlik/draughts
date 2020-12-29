a1d32b9eb9a0fff090eb1914dda31e6f08007be1
Jiri Petrlik <jiripetrlik@gmail.com>
2020-12-29 16:38:58 +0100

    Add story for NameInput

---
+++ frontend/src/components/NameInput/NameInput.stories.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NameInput from './NameInput'
+
+export default {
+  component: NameInput,
+  title: 'NameInput',
+};
+
+const Template = args => <NameInput/>;
+
+export const Default = Template.bind({});
